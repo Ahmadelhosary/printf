c7d880aa15667fcd3036fce9890b8f6abab54455
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-07-04 17:02:13 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	}
 	i = 0, count = 0;
-	for (i = 0; format && format[i] != '\0'; i++)
+	while (format && format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
